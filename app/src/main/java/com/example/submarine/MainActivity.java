7386a4176b4a640ffb17925993d16de5f065b0ff
haschish <haschish@gmail.com>
2020-04-03 14:30:57 +0500

    step 1

--- app/src/main/java/com/example/submarine/MainActivity.java
+++ app/src/main/java/com/example/submarine/MainActivity.java
@@ -3,12 +3,68 @@ package com.example.submarine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+     * блок настройки
+     * */
+    int shots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        initGame();
+        drawGrid();
+    }
+
+    /*
+     * блок выбора случайного положения лодки
+     * */
+    void initGame() {
+        Log.d("test", "initGame");
+        shots = 0;
+    }
+
+    /*
+     * блок отрисовки сетки
+     * */
+    void drawGrid() {
+        Log.d("test", "drawGrid");
+        Log.d("test", "renderShots: " + shots);
+    }
+
+    /*
+     * отработка нажатия
+     * */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("test", "onTouchEvent");
+        onHit();
+        return true;
+//        return super.onTouchEvent(event);
+
+    }
+    /*
+     *
+     * */
+    void onHit() {
+        shots++;
+        Log.d("test","onHit");
+        if (Math.random() > .7f) {
+            boom();
+        } else {
+            drawGrid();
+        }
+    }
+
+    /*
+     * блок показа Boom!
+     * */
+    void boom() {
+        Log.d("test", "Boom!");
+        initGame();
     }
 }
